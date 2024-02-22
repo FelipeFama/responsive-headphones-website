2e9482fe4ce8905d9829bcfa2e8d68387e1af5c6
FelipeFama <lipehfama@gmail.com>
2024-02-21 21:48:04 -0300

    feat: add new componet and new package

---
+++ src/components/Favorite/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.scss";
+
+export default function Favorite() {
+  return (
+    <section>
+      <div></div>
+    </section>
+  );
+}
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,8 @@
 import aboutImg from "../../public/img/about-img.png";
+import favoriteOneImg from "../../public/img/favorite-1.png";
+import favoriteTwoImg from "../../public/img/favorite-2.png";
+import favoriteThreeImg from "../../public/img/favorite-3.png";
+import favoriteFourImg from "../../public/img/favorite-4.png";
 import homeImg from "../../public/img/home-img.png";
 import toolTipImg from "../../public/img/tooltip-right.svg";
 
@@ -19,6 +23,24 @@ export const assetsProject = {
     img: {
       source: aboutImg,
       alt: "image",
+    },
+  },
+  favorite: {
+    favoriteOneImage: {
+      source: favoriteOneImg,
+      alt: "favorite",
+    },
+    favoriteTwoImage: {
+      source: favoriteTwoImg,
+      alt: "favorite",
+    },
+    favoriteThreeImage: {
+      source: favoriteThreeImg,
+      alt: "favorite",
+    },
+    favoriteFourImage: {
+      source: favoriteFourImg,
+      alt: "favorite",
     },
   },
 };
