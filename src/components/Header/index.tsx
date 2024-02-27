8c05f2f277dc57fbbff3b5cfe6d9b677e1976a4d
FelipeFama <lipehfama@gmail.com>
2024-02-26 21:33:54 -0300

    feat: add scroll feature in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RiCloseLine, RiMenuLine } from "react-icons/ri";
 import "./styles.scss";
 
@@ -12,6 +12,46 @@ export default function Header() {
   const closeMenu = () => {
     setMenuOpen(false);
   };
+
+  const blurHeader = () => {
+    const header = document.getElementById("header");
+    window.scrollY >= 50
+      ? header?.classList.add("blur-header")
+      : header?.classList.remove("blur-header");
+  };
+
+  const scrollActive = () => {
+    const sections = document.querySelectorAll("section[id]");
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+      const sectionHeight = (current as HTMLElement).offsetHeight;
+      const sectionTop = (current as HTMLElement).offsetTop - 58;
+      const sectionId = (current as HTMLElement).getAttribute("id");
+      const sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]",
+      );
+
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass?.classList.add("active-link");
+      } else {
+        sectionsClass?.classList.remove("active-link");
+      }
+    });
+  };
+
+  const handleScrollHeader = () => {
+    blurHeader();
+    scrollActive();
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollHeader);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollHeader);
+    };
+  }, []);
 
   return (
     <header className="header" id="header">
